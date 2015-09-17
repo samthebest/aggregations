028edc8ebdd09ab06b1ded61403c57bd8eb24eea
samthebest <samthesavage@gmail.com>
2015-09-17 14:54:14 +0100

    Error estimator ready

--- src/main/scala/sam/aggregations/ErrorEstimator.scala
+++ src/main/scala/sam/aggregations/ErrorEstimator.scala
@@ -1,19 +1,33 @@
 package sam.aggregations
 
+import breeze.stats.distributions.Gaussian
+
 import scala.util.Random
 
 object ErrorEstimator {
   val rand = new Random()
+
+  def normalishSample(n: Int, max: Int): IndexedSeq[Long] = {
+    val dist = new Gaussian(max / 2.0, max / 6.0)
+    (1 to n).map(_ => math.floor(dist.draw())).map {
+      case i if i < 0 => 0L
+      case i if i > max => max.toLong
+      case i => i.toLong
+    }
+  }
+
+  def normalDistribution(median:  Median, n: Int, max: Int): Double = relativeError(normalishSample(n, max), median)
+
   def uniformDistribution(median: Median, n: Int, max: Int): Double =
     relativeError((1 to n).map(_ => rand.nextInt(max).toLong), median)
 
-  def correctMedian(numbers: IndexedSeq[Long]): Double = {
+  def correctMedian(numbers: Seq[Long]): Double = {
     val exactMedian = new ExactMedian()
     numbers.foreach(exactMedian.update)
     exactMedian.result
   }
 
-  def relativeError(numbers: IndexedSeq[Long], median: Median): Double = {
+  def relativeError(numbers: Seq[Long], median: Median): Double = {
     val correct = correctMedian(numbers)
     numbers.foreach(median.update)
     val medianEstimate = median.result
--- src/test/scala/sam/aggregations/ErrorEstimatorSpecs.scala
+++ src/test/scala/sam/aggregations/ErrorEstimatorSpecs.scala
@@ -3,9 +3,34 @@ package sam.aggregations
 import org.specs2.mutable.Specification
 
 class ErrorEstimatorSpecs extends Specification {
+  def mockMedian(fixedResult: Double): Median = new Median {
+    def result: Double = fixedResult
+    def update(e: Long): Unit = ()
+    def update(m: Median): Unit = ()
+  }
+
   "ErrorEstimator" should {
+    "Correctly estimate 10% error for mock median" in {
+      val numbers = Seq(1, 3, 50, 50, 60, 88).map(_.toLong)
+      ErrorEstimator.relativeError(numbers, mockMedian(55.0)) must_=== 0.1
+    }
+
+    "Correctly estimate 5% error for mock median" in {
+      val numbers = Seq(1, 3, 50, 50, 60, 88).map(_.toLong)
+      ErrorEstimator.relativeError(numbers, mockMedian(52.5)) must_=== 0.05
+    }
+
+    "Correctly estimate 0% error for mock median" in {
+      val numbers = Seq(1, 3, 50, 50, 60, 88).map(_.toLong)
+      ErrorEstimator.relativeError(numbers, mockMedian(50.0)) must_=== 0.0
+    }
+
     "Correctly estimate zero error for sampling from a uniform distribution for the ExactMedian" in {
-      ErrorEstimator.uniformDistribution(new ExactMedian(), 1000, 100) must_=== 0.0
+      ErrorEstimator.uniformDistribution(new ExactMedian(), 100, 10) must_=== 0.0
+    }
+
+    "Correctly estimate zero error for sampling from a normal distribution for the ExactMedian" in {
+      ErrorEstimator.normalDistribution(new ExactMedian(), 100, 10) must_=== 0.0
     }
   }
 }
