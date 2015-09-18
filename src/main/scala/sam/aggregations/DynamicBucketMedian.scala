2015723247d335bb8acb343fd3c8c60e0b16dedf
samthebest <samthesavage@gmail.com>
2015-09-18 07:00:56 +0100

    started DynamicBucketMedian

--- src/main/scala/sam/aggregations/DynamicBucketMedian.scala
+++ src/main/scala/sam/aggregations/DynamicBucketMedian.scala
@@ -2,7 +2,21 @@ package sam.aggregations
 
 import scala.collection.mutable
 
-class DynamicBucketMedian(maxMapSize: Int) extends Median {
+object DynamicBucketMedian {
+  def addThenMerge(maxMapSize: Int, balanceRatio: Double, m: mutable.Map[(Long, Long), Long], e: Int): Unit = {
+
+    // Something like:
+    // find the center of mass
+    // find the half way point (sort by values)
+    // This will determine which side is unbalanced
+    // Starting from the end point, find the first value that if merged it's size would not exceed the end-point
+    // if no such point exists, then merge the end point
+
+    // Alternate algo: when full, use max and min to half the number of points in the map by chopping into equal ranges
+  }
+}
+
+class DynamicBucketMedian(maxMapSize: Int, balanceRatio: Double) extends Median {
   // first implementation will use a mutable map, should be replaced with custom Array based implementation
 
   val m: mutable.Map[(Long, Long), Long] = mutable.Map.empty
--- src/test/scala/sam/aggregations/DynamicBucketMedianSpec.scala
+++ src/test/scala/sam/aggregations/DynamicBucketMedianSpec.scala
@@ -1,5 +1,28 @@
 package sam.aggregations
 
-class DynamicBucketMedianSpec extends MedianSpecUtils {
+import scala.collection.mutable
 
+class DynamicBucketMedianSpec extends MedianSpecUtils {
+  def toLongMap(m: mutable.Map[(Int, Int), Int]): mutable.Map[(Long, Long), Long] =
+    m.map {
+      case ((r1, r2), count) => ((r1.toLong, r2.toLong), count.toLong)
+    }
+
+  "DynamicBucketMedian object" should {
+    "Update a map and merge the correct buckets" in {
+      val maxMapSize: Int = 2
+      val balanceRatio: Double = 0.8
+      val m: mutable.Map[(Long, Long), Long] = toLongMap(mutable.Map(
+        (0, 0) -> 1,
+        (1, 1) -> 1
+      ))
+
+      DynamicBucketMedian.addThenMerge(maxMapSize, balanceRatio, m, 0)
+
+      m.toMap must_=== toLongMap(mutable.Map(
+        (0, 0) -> 2,
+        (1, 1) -> 1
+      )).toMap
+    }
+  }
 }
