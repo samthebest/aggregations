cf4c4f5a932951621817e1be5f760e73f9918c60
samthebest <samthesavage@gmail.com>
2015-09-24 18:01:25 +0100

    Nearly finished DSL, needs a special & function

--- src/main/scala/sam/aggregations/Aggregator.scala
+++ src/main/scala/sam/aggregations/Aggregator.scala
@@ -25,14 +25,20 @@ trait Aggregator[+R, V, A <: Aggregator[R, V, A]] { self: A =>
 
   def update(e: V*): Unit = Seq(e: _*).foreach(update)
 
-  def &[B <: Aggregator[_, V, B]](b: B) =
-    &&[V, A, MultiAggregator[V]](this, &&[V, B, MultiAggregator[V]](b, MultiAggregatorNil[V]()))
+//  def &[B <: Aggregator[_, V, B]](b: B): MultiAggregator[V] =
+//    &&[V, A, MultiAggregator[V]](this, &&[V, B, MultiAggregator[V]](b, MultiAggregatorNil[V]()))
+
+  // This complex pattern matching is to ensure when we call result on a MultiAggregator we get a nice HList
+//  def &[B <: Aggregator[_, V, B]](b: B): MultiAggregator[V] = b match {
+//    case ma: MultiAggregator[V] => &&[V, A, MultiAggregator[V]](this, ma)
+//    case _ => &&[V, A, MultiAggregator[V]](this, &&[V, B, MultiAggregator[V]](b, MultiAggregatorNil[V]()))
+//  }
+
+  def &[B <: Aggregator[_, V, B]](b: B): MultiAggregator[V] = b match {
+    case ma: MultiAggregator[V] => &&[V, A, MultiAggregator[V]](this, ma)
+    case _ => &&[V, A, MultiAggregator[V]](this, &&[V, B, MultiAggregator[V]](b, MultiAggregatorNil[V]()))
+  }
 }
-
-// TODO Finish off this DSL - will be uber cool when finished.
-
-// MultiAggregator should extend Aggregator, where the type of V is fixed, R is a "HList" (a MultiResult, no need to
-// make shapeless a dependency for just one thing)
 
 trait MultiResult extends Product with Serializable
 
@@ -41,20 +47,35 @@ case object MultiResultNil extends MultiResult
 final case class RR[+H, T <: MultiResult](head: H, tail: T) extends MultiResult
 
 sealed trait MultiAggregator[V] extends Product with Serializable with Aggregator[MultiResult, V, MultiAggregator[V]] {
-  def update(e: V): Unit = ???
-  def update(a: MultiAggregator[V]): Unit = ???
-  def result: MultiResult = ???
-}
+  // TODO We need to override & to make it so that the head is never a MutliAggregator - otherwise we will end up
+  // with an unnecessary level of nesting
 
-case object MultiAggregatorNil extends MultiAggregator[Nothing]
+//  override def &[B <: Aggregator[_, V, B]](b: B): MultiAggregator[V] = b match {
+//    case ma: MultiAggregator[V] => &&[V, MultiAggregator[V], MultiAggregator[V]](this, ma)
+//    case _ => &&[V, MultiAggregator[V], MultiAggregator[V]](this, &&[V, B, MultiAggregator[V]](b, MultiAggregatorNil[V]()))
+//  }
+}
 
 case class MultiAggregatorNil[V]() extends MultiAggregator[V] {
-  override def update(e: V): Unit = ()
-  override def update(a: MultiAggregator[V]): Unit = ()
-  override def result: MultiResult = MultiResultNil
+  def update(e: V): Unit = ()
+  def update(a: MultiAggregator[V]): Unit = ()
+  def result: MultiResult = MultiResultNil
 }
 
-final case class &&[V, H <: Aggregator[_, V, H], T <: MultiAggregator[V]](head: H, tail: T) extends MultiAggregator[V]
+final case class &&[V, H <: Aggregator[_, V, H], T <: MultiAggregator[V]](head: H, tail: T) extends MultiAggregator[V] {
+  def update(e: V): Unit = {
+    head.update(e)
+    tail.update(e)
+  }
+
+  def update(a: MultiAggregator[V]): Unit = a match {
+    case &&(headA: H, tailA: T) =>
+      head.update(headA)
+      tail.update(tailA)
+  }
+
+  def result: MultiResult = RR(head.result, tail.result)
+}
 
 object Aggregator {
   implicit class PimpedRDD[K: ClassTag, V: ClassTag](rdd: RDD[(K, V)]) {
