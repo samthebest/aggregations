910aba0f1d6f8afd8ec61783dba0bcc0c4d95be0
Sam Savage <sam.savage@mendeley.com>
2015-12-17 17:13:13 +0000

    Formalized definition of nthTile

--- src/main/scala/sam/aggregations/Utils.scala
+++ src/main/scala/sam/aggregations/Utils.scala
@@ -4,9 +4,12 @@ import scala.collection.immutable.Nil
 import scala.reflect.ClassTag
 
 object Utils {
-  def nthTileMapper[T](n: Int, tToCount: Map[T, Long]) = {
+  /**The resulting mapper will tell us the numerator X of the highest fraction X / n such that X / n values fall
+    * strictly below. See tests for examples.*/
+  def nthTileMapper[T](n: Int, tToCount: Map[T, Long])(implicit ordering: Ordering[T]): T => Option[Int] = {
     require(n > 1, "n must be greater than 1")
     require(n <= tToCount.size, s"Cannot define nth-tiles with less data points (${tToCount.size}) than n ($n)")
+    tToCount.keys.toList.sorted.zipWithIndex.toMap.get
   }
 
   def cumulativeDensity[T](tToCount: List[(T, Long)]): List[(T, Long)] =
